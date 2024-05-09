fa3487f8288774b4a0a4f7115dbb3c179833d77d
Shahriyor <zxcnanevermore@gmail.com>
2024-05-09 21:08:45 +0500

    done Algorithm6

---
+++ algorithm6.py
@@ -0,0 +1,67 @@
+#1
+# def number_split(num):
+#     nasm = num // 2
+#     if num % 2 != 0:
+#         return [nasm, nasm + 1]
+#     else:
+#         return [nasm, nasm]
+#
+#
+# print(number_split(4))
+# print(number_split(10))
+# print(number_split(11))
+# print(number_split(-9))
+
+#2
+# def nums(lst,s):
+#     if s == "Asc":
+#         return sorted(lst)
+#     elif s == "Des":
+#         return sorted(lst,reverse=True)
+#     elif s == "None":
+#         return lst
+#
+#
+# print(nums([4,3,2,1], "Asc"))
+# print(nums([7,8,11,66],"Des"))
+# print(nums([1,2,3,4],"None"))
+
+
+
+#3
+#1 Yoli
+# def resistance(resistances):
+#     total_resistance = 0
+#     for resistance in resistances:
+#         total_resistance += resistance
+#     return f"{total_resistance} Om"
+#
+#
+# print(resistance([1, 5, 6, 3]))
+# print(resistance([16, 3.5, 6]))
+# print(resistance([0.5, 0.5]))
+
+#2 Yoli
+# def numbers(lst):
+#     a = sum(lst)
+#     if a > 1:
+#         return f"{a} ohms"
+#     elif a == 1:
+#         return f"{a} ohm"
+#
+#
+# print(numbers([1,5,6,3]))
+# print(numbers([16,3.5,6]))
+# print(numbers([0.5,0.5]))
+
+
+
+
+
+
+
+
+
+
+
+
